f28a71aff12199f89c7269b8296882ba2fc01650
Tatsuya Takigawa <taki0701tty@gmail.com>
2025-05-25 15:13:46 +0900

    統合に伴い削除

--- app/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_bp = Blueprint("home", __name__)  # "home" という名前のBlueprintを定義
-
-@home_bp.route("/home")
-def home():
-    return render_template("home.html")
